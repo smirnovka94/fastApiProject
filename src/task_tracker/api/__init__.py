06a15bb21b1401d1e72293ae77394305c2851d5a
lekseich8 <lekseich8@gmail.com>
2023-12-27 22:55:49 +0300

    Destroy routers to 2 files

--- src/task_tracker/api/__init__.py
+++ src/task_tracker/api/__init__.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 from .routers import router as operation_router
+from .functions import router as functions
+
 
 router = APIRouter()
 router.include_router(operation_router)
+router.include_router(functions)
